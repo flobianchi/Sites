63d4157da3c65dd85af6dadd11e29300a39c2d6c
Victoria <vacontardo@uc.cl>
2021-06-24 11:54:52 -0400

    reseteo viki

--- config/data.php
+++ config/data.php
@@ -1,5 +1,9 @@
 <?php
   $user = 'grupo53';
   $password = 'grupo53';
-  $databaseName = 'grupo53e2';
+  $databaseName = 'grupo53e3';
+
+  $user2 = 'grupo56';
+  $password2 = 'grupo56';
+  $databaseName2 = 'grupo56e3';
 ?>
