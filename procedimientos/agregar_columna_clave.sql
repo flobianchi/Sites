a7f6a7889999d7d682f53f714a11796fa071d7ca
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-06-25 12:54:50 -0400

    procedimiento columna clave

---
+++ procedimientos/agregar_columna_clave.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION
+
+agregar_columna_clave ()
+
+-- declaramos lo que retorna, en este caso un booleano
+RETURNS BOOLEAN AS $$
+
+-- definimos nuestra función
+BEGIN
+
+    -- verificar si existe la columna clave, si no existe la agregamos y seteamos todos los valores de esa columna en 1
+    IF 'clave' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
+        ALTER TABLE usuarios ADD clave VARCHAR(30);
+        UPDATE usuarios SET clave = '1';
+        RETURN TRUE;
+    ELSE
+        RETURN FALSE;
+    END IF;
+
+END
+$$ language plpgsql
