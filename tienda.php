75766948f76fd0aa0aa2aa6f0f4d84062471cac9
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-06-25 10:28:04 -0400

    tabla tiendas

--- tienda.php
+++ tienda.php
@@ -7,7 +7,50 @@
 <form id = 'caja' action="consultas_tienda.php" method="post">
       <input type="submit" value="Elegir tienda" id = "botonB">
       </form>
-Mostrar aqui tabla con botones que mande al wp
+
+
+<?php
+#Llama a conexión, crea el objeto PDO y obtiene la variable $db
+require("../config/conexion.php");
+
+$query = "SELECT t.id, t.nombre FROM tiendas AS t ORDER BY t.id;";
+
+$result = $db -> prepare($query);
+$result -> execute();
+$dataCollected = $result -> fetchAll(); #Obtiene todos los resultados de la consulta en forma de un arreglo
+
+echo(
+"Test tabla clickeable.
+<br>
+<br>
+Muestra todas las tiendas y tiene ID clickeable
+
+</div>
+<div></div>
+<div></div> 
+</div>
+
+<p style='font-size:50px;'></p>
+<table class='center'>
+<tr>
+<th>id tienda</th>
+<th>Tienda</th>
+</tr>");
+
+
+foreach ($dataCollected as $p) {
+echo "<tr> <td> 
+
+<form id = 'caja' action='resultado.php' method='post'>
+<input name = 't' type='submit' value='$p[0]' id = 'botonID'>
+</form>
+
+
+</td> <td>$p[1]</td> </tr>";
+}
+
+echo("<table>");
+
 <br>
 
 <br>
