43ac4df8cadb12bc85333f2b87161e069a0f6d3a
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-06-25 15:07:51 -0400

    Update claves_random.php

--- procedimientos/claves_random.php
+++ procedimientos/claves_random.php
@@ -3,6 +3,17 @@
     require("../config/conexion.php");
     include('../templates/header_procedimientos.html');
 
+    #https://stackoverflow.com/questions/4356289/php-random-string-generator
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
     echo("
     <br>
     <h1>Creador de claves random</h1>
@@ -14,10 +25,9 @@
     $result -> execute();
     $respuesta = $result -> fetchAll();
 
-    print_r($respuesta);
-
     foreach ($respuesta as $p) {
-        $query2 = "SELECT cambiar_clave($p[0],'2');";
+        $clave = generateRandomString(8);
+        $query2 = "SELECT cambiar_clave($p[0],$clave);";
         $result2 = $db -> prepare($query2);
         $result2 -> execute();
       }
