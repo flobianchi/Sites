0046f330d750f4cf027e414b208967afc4185e53
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-06-25 14:50:00 -0400

    Create cambiar_clave.sql

---
+++ procedimientos/cambiar_clave.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION
+
+cambiar_clave (id_usuario INT, nueva_clave VARCHAR)
+
+-- declaramos lo que retorna, en este caso un booleano
+RETURNS BOOLEAN AS $$
+
+-- definimos nuestra función
+BEGIN
+
+    UPDATE usuarios SET clave = nueva_clave WHERE id = id_usuario
+
+END
+$$ language plpgsql
