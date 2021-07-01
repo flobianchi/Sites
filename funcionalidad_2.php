deba33b145a93962b92160abb806f5279e59c865
flobianchi <fbianchi1@uc.cl>
2021-07-01 10:14:21 -0400

    session

--- funcionalidad_2.php
+++ funcionalidad_2.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <?php include('templates/header.html');   ?>
 <?php include('templates/header_botones.html');   ?>
 
@@ -10,9 +11,41 @@ Consultar productos aqui
       </form>
 
   <br>
+  <?php session_start(); ?>
   <form id = 'caja' action="consultas_tienda.php" method="post">
   <input type="submit" value="Volver" id = "botonB">
   </form>
+
+  <?php
+
+  $id = $_SESSION['id_tienda'];
+
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("config/conexion.php");
+
+ $query = "SELECT productos.id, productos.nombre, productos.precio FROM productos JOIN disponibilidad_tienda ON productos.id = disponibilidad_tienda.id_producto WHERE productos.nombre LIKE ('%$input%');";
+ $result = $db -> prepare($query);
+	$result -> execute();
+	$unidades = $result -> fetchAll();
+
+  # FALTA AGREGAR EL TIPO!!!!!!!!!!!!!!!!!!!!!!!!!! preguntar martin
+
+  ?>
+
+	<table>
+    <tr>
+      <th>ID</th>
+      <th>Nombre</th>
+      <th>Precio</th>
+    </tr>
+  <?php
+	foreach ($unidades as $unidad) {
+  		echo "<tr><td>$unidad[0]</td><td>$unidad[1]</td> <td>$unidad[2]</td></tr>";
+	}
+  ?>
+	</table>
+
+<?php include('templates/footer.html'); ?>
 
 <br>
 DD de MM del 2021
