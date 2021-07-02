1f2a528bc37600551a6a17fbeba4f82ca556a9ba
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-07-02 17:31:01 -0400

    Update historial.php

--- historial.php
+++ historial.php
@@ -25,7 +25,7 @@ Mostar historial de compras aqui (carrito)
         // Super JOIN
         //$query = "SELECT fecha, nombre, cantidad, id_tienda FROM (fechas JOIN carrito_compras ON id_compra = id_compra) JOIN productos AS p ON id_producto = p.id WHERE fecha = $fecha_compra;";
         echo("<br> $fecha_compra[1] </br>");
-        $query = "SELECT * FROM carrito_compras AS cc JOIN compras AS c ON cc.id_compra = c.id WHERE c.id = $fecha_compra[1] AND cc.id_usuario = $id_current_user;";
+        $query = "SELECT * FROM carrito_compras AS cc JOIN compras AS c ON cc.id_compra = c.id WHERE c.id = $fecha_compra[1] AND c.id_usuario = $id_current_user;";
 
         // Ejecutamos las querys 
         $result = $db -> prepare($query);
