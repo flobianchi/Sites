74cecc52308d223781791377580342d657afe158
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-07-02 18:32:09 -0400

    Update perfil.php

--- perfil.php
+++ perfil.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
-<?php include('login.php');   ?>
+<?php include('config/login.php');   ?>
 <?php include('templates/header.html');   ?>
 <?php include('templates/header_botones.html');   ?>
 
