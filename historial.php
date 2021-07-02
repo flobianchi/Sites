75bc1d700c42096ce89d352d0d89500fb917aecb
Victoria Contardo <vacontardo@uc.cl>
2021-07-02 17:03:34 -0400

    prueba historial

--- historial.php
+++ historial.php
@@ -1,7 +1,43 @@
+<?php session_start(); ?>
 <?php include('templates/header.html');   ?>
 <?php include('templates/header_botones.html');   ?>
 
+<body>  
 Mostar historial de compras aqui (carrito)
+
+<?php
+
+    $id_current_user = $_SESSION['id_user'];
+
+    // Nos conectamos a las bdds
+    require("config/conexion.php");
+
+    // Primero obtenemos las fechas de las compras de la tabla DESPACHOS de bd2
+    $query = "SELECT fecha, id_compra FROM Despachos ORDER BY fecha;";
+    $result = $db2 -> prepare($query);
+    $result -> execute();
+    $fechas = $result -> fetchAll();
+
+
+    foreach ($fechas as $fecha_compra){
+
+        // Super JOIN
+        //$query = "SELECT fecha, nombre, cantidad, id_tienda FROM (fechas JOIN carrito_compras ON id_compra = id_compra) JOIN productos AS p ON id_producto = p.id WHERE fecha = $fecha_compra;";
+
+        $query = "SELECT * FROM carrito_compras AS cc JOIN compras AS c ON cc.id_compra = c.id WHERE c.id = $fecha_compra[1] AND cc.id_usuario = $id_current_user;";
+
+        // Ejecutamos las querys 
+        $result = $db -> prepare($query);
+        $result -> execute();
+        $result -> fetchAll();
+        echo("<br>$fecha_compra[0]");
+        print_r($result);
+        echo("<\br>");
+    }
+
+
+?>
+
 
 <form id = 'caja' action="perfil.php" method="post">
       <input type="submit" value="Volver" id = "botonB">
