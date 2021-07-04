235ec1b95d0deef5099629cf4c51c80f659e7d66
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-07-04 17:44:00 -0400

    Update agregar_columna_clave.sql

--- procedimientos/agregar_columna_clave.sql
+++ procedimientos/agregar_columna_clave.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION
 
-agregar_columna_clave (entrada int)
+agregar_columna_clave ()
 
 -- declaramos lo que retorna, en este caso un booleano
 RETURNS BOOLEAN AS $$
@@ -11,7 +11,7 @@ BEGIN
     -- verificar si existe la columna clave, si no existe la agregamos y seteamos todos los valores de esa columna en 1
     IF 'clave' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
         ALTER TABLE usuarios ADD clave VARCHAR(30);
-        UPDATE usuarios SET clave = '1';
+        UPDATE usuarios SET clave = 'cambiar_clave';
         RETURN TRUE;
     ELSE
         RETURN FALSE;
