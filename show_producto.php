2d521513aed912f621b450debfd828bc0a659ef6
flobianchi <fbianchi1@uc.cl>
2021-07-03 21:13:30 -0400

    tabla p

--- show_producto.php
+++ show_producto.php
@@ -65,7 +65,7 @@ Mostrar aqui atributos del producto
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$unidades = $result -> fetchAll();
-echo('<table>
+echo('<table class="center">
 <tr>');
 $fila = '<tr>';
 
