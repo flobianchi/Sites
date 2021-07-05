49f3a96fd3094b059cfa681b49b02fafe1c18287
sjdiaz1 <44927163+sjdiaz1@users.noreply.github.com>
2021-07-05 02:26:43 -0400

    claves_columna

--- procedimientos/agregar_columna.php
+++ procedimientos/agregar_columna.php
@@ -4,25 +4,26 @@
     require("../config/conexion.php");
     include('../templates/header_procedimientos.html');
 
-    echo("<br>Ejecutando procedimiento agregar_columna_clave()<br>");
+    echo("<br>Ejecutando procedimiento agregar_columna_clave()<br>Este procedimiento agrega el atributo clave en la realacion usuarios solo si no existe <br>");
 
-    $query = "SELECT agregar_columna_clave(1);";
-
-    $result = $db -> prepare($query);
-    $result -> execute();
-    $respuesta = $result -> fetchAll();
-
-    print_r($respuesta);
-
-    echo("<br>Ejecutando procedimiento agregar_columna_calificacion()<br>");
-
-    $query = "SELECT agregar_columna_calificacion(1);";
+    $query = "SELECT agregar_columna_clave();";
 
     $result = $db -> prepare($query);
     $result -> execute();
     $respuesta = $result -> fetchAll();
 
-    print_r($respuesta);
+    $se_agrego = $respuesta[0]['agregar_columna_clave'];
+    echo("se agrego una columna? -> $se_agrego");
+
+    #echo("<br>Ejecutando procedimiento agregar_columna_calificacion()<br>");
+
+    #$query = "SELECT agregar_columna_calificacion(1);";
+
+    #$result = $db -> prepare($query);
+    #$result -> execute();
+    #$respuesta = $result -> fetchAll();
+
+    #print_r($respuesta);
 
 ?>
 
